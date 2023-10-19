3d3b35dd00b5038f676a4b4a094082e346bd6b5b
Atrabilis <angel.bruno.andrade.osorio@gmail.com>
2023-10-19 17:07:49 -0230

    Added Graphs for errors and anti windup feature

--- PID/PID.ino
+++ PID/PID.ino
@@ -1,96 +1,87 @@
 #include <Arduino.h>
 
-#define ENCA 2 // YELLOW
-#define ENCB 3 // WHITE
+#define ENCA 2
+#define ENCB 3
 #define PWM 5
 #define IN2 6
 #define IN1 7
 
-volatile int posi = 0; 
+volatile int posi = 0;
 long prevT = 0;
 float eprev = 0;
 float eintegral = 0;
-const int maxIntegral = 500; // Define el límite máximo para el valor integral
+const int maxIntegral = 20;
+bool antiWindupEnabled = true;
+
+float kp = 5; // Valores iniciales para los parámetros PID
+float ki = 3;
+float kd = .5;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(ENCA,INPUT);
-  pinMode(ENCB,INPUT);
-  attachInterrupt(digitalPinToInterrupt(ENCA),readEncoder,RISING);
-  
-  pinMode(PWM,OUTPUT);
-  pinMode(IN1,OUTPUT);
-  pinMode(IN2,OUTPUT);
-  
-  Serial.println("target pos");
+  pinMode(ENCA, INPUT);
+  pinMode(ENCB, INPUT);
+  attachInterrupt(digitalPinToInterrupt(ENCA), readEncoder, RISING);
+
+  pinMode(PWM, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+
+  Serial.println("Ready for PID parameters.");
 }
 
 void loop() {
-  // set target position
-  int target = 300; // Cambiado de 1200 a 300 para propósitos de este ejemplo
-
-  // PID constants
-  float kp = 5;
-  float kd = .5;
-  float ki = 3;
-
-  // time difference
-  long currT = micros();
-  float deltaT = ((float) (currT - prevT)) / (1e6);
-  prevT = currT;
-
-  // Read the position
-  int pos = 0; 
-  noInterrupts(); // disable interrupts temporarily while we read posi
-  pos = posi;
-  interrupts(); // enable interrupts again
-  
-  // error
-  int e = pos - target;
-
-  // derivative
-  float dedt = (e - eprev) / deltaT;
-
-  // integral with anti-windup
-  eintegral += e * deltaT;
-  if (eintegral > maxIntegral) { // Si el valor integral excede el máximo permitido,
-    eintegral = maxIntegral; // lo limitamos al máximo.
-  } else if (eintegral < -maxIntegral) { // De manera similar, si está por debajo del mínimo,
-    eintegral = -maxIntegral; // lo limitamos al mínimo.
+  if (Serial.available() > 0) {
+    String receivedData = Serial.readStringUntil('\n');
+    parseAndUpdatePID(receivedData);
   }
 
-  // control signal
+  int target = 300;
+
+  long currT = micros();
+  float deltaT = ((float)(currT - prevT)) / (1e6);
+  prevT = currT;
+
+  int pos = 0;
+  noInterrupts();
+  pos = posi;
+  interrupts();
+
+  int e = pos - target;
+
+  float dedt = (e - eprev) / deltaT;
+
+  eintegral += e * deltaT;
+  if (antiWindupEnabled) {
+    if (eintegral > maxIntegral) {
+      eintegral = maxIntegral;
+    } else if (eintegral < -maxIntegral) {
+      eintegral = -maxIntegral;
+    }
+  }
+
   float u = kp * e + kd * dedt + ki * eintegral;
 
-  // motor power
   float pwr = fabs(u);
   if (pwr > 255) {
     pwr = 255;
   }
 
-  // motor direction
-  int dir = 1;
-  if (u < 0) {
-    dir = -1;
-  }
+  int dir = (u < 0) ? -1 : 1;
 
-  // signal the motor
   setMotor(dir, pwr, PWM, IN1, IN2);
 
-  // store previous error
   eprev = e;
 
   Serial.print(target);
   Serial.print(" ");
   Serial.print(pos);
-  //Serial.print(" ");
-  //Serial.print(e);
-  //Serial.print(" ");
-  //Serial.print(eintegral);
-  //Serial.print(" ");
-  //Serial.print(dedt);
-  Serial.println();
-  
+  Serial.print(" ");
+  Serial.print(e);
+  Serial.print(" ");
+  Serial.print(dedt);
+  Serial.print(" ");
+  Serial.println(eintegral);
 }
 
 void setMotor(int dir, int pwmVal, int pwm, int in1, int in2) {
@@ -109,9 +100,47 @@ void setMotor(int dir, int pwmVal, int pwm, int in1, int in2) {
 
 void readEncoder() {
   int b = digitalRead(ENCB);
-  if (b > 0) {
-    posi++;
-  } else {
-    posi--;
+  posi += (b > 0) ? 1 : -1;
+}
+
+void parseAndUpdatePID(String data) {
+  char inputChars[data.length() + 1];
+  data.toCharArray(inputChars, sizeof(inputChars));
+  char* ptr = NULL;
+  float newPidValues[3] = {kp, ki, kd}; // Inicializa con valores actuales
+  bool pidValuesReceived[3] = {false, false, false}; // Monitorea qué valores se recibieron
+  bool windupCommandReceived = false;
+
+  ptr = strtok(inputChars, ",");
+  while (ptr != NULL) {
+    String pidValue(ptr);
+    if (pidValue.startsWith("P") && pidValue.length() > 1) {
+      newPidValues[0] = pidValue.substring(1).toFloat();
+      pidValuesReceived[0] = true;
+    } else if (pidValue.startsWith("I") && pidValue.length() > 1) {
+      newPidValues[1] = pidValue.substring(1).toFloat();
+      pidValuesReceived[1] = true;
+    } else if (pidValue.startsWith("D") && pidValue.length() > 1) {
+      newPidValues[2] = pidValue.substring(1).toFloat();
+      pidValuesReceived[2] = true;
+    } else if (pidValue.startsWith("W")) {
+      antiWindupEnabled = pidValue.substring(1).toInt() == 1;
+      windupCommandReceived = true;
+    }
+    ptr = strtok(NULL, ",");
+  }
+
+  // Actualiza los valores si se recibieron nuevos
+  if (pidValuesReceived[0] || pidValuesReceived[1] || pidValuesReceived[2]) {
+    kp = newPidValues[0];
+    ki = newPidValues[1];
+    kd = newPidValues[2];
+    Serial.print("PID updated - P: "); Serial.print(kp);
+    Serial.print(", I: "); Serial.print(ki);
+    Serial.print(", D: "); Serial.println(kd);
+  }
+
+  if (windupCommandReceived) {
+    Serial.print("Anti-windup command received - Status: "); Serial.println(antiWindupEnabled ? "Enabled" : "Disabled");
   }
 }
--- interfaz.py
+++ interfaz.py
@@ -1,29 +1,82 @@
 import sys
 import serial
 import time
-from PyQt5 import QtWidgets, QtCore
-import pyqtgraph as pg
 import collections
 import threading
+from PyQt5 import QtWidgets, QtCore
+import pyqtgraph as pg
 
 SERIAL_PORT = 'COM3'
 BAUD_RATE = 9600
 MAX_POINTS = 50
 
-class RealTimeGraph:
-    def __init__(self):
-        self.app = QtWidgets.QApplication(sys.argv)
-        self.win = pg.GraphicsLayoutWidget(show=True)
-        self.win.setWindowTitle('Real Time Graph')
-        self.plot = self.win.addPlot()
-        self.plot.setLabels(left='Position', bottom='Time', title='Reference vs Actual Position')
-        self.plot.addLegend()
+class RealTimeGraph(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
+        # Configuración de la interfaz gráfica de usuario
+        self.init_ui()
+
+        # Inicialización de la conexión serial y el thread de lectura
+        self.serial = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
+        time.sleep(2)  # Espera para la inicialización del puerto serial
+        self.serial.flushInput()
+
+        self.serial_thread = threading.Thread(target=self.read_from_port, args=(self.serial,))
+        self.serial_thread.daemon = True
+        self.serial_thread.start()
+
+        # Variables para guardar los datos leídos
+        self.latest_data = None
+        self.lock = threading.Lock()
+
+    def init_ui(self):
+        self.layout = QtWidgets.QVBoxLayout(self)
+
+        # Parámetros PID
+        self.pid_layout = QtWidgets.QHBoxLayout()
+        self.p_input = QtWidgets.QLineEdit(self)
+        self.i_input = QtWidgets.QLineEdit(self)
+        self.d_input = QtWidgets.QLineEdit(self)
+        self.windup_checkbox = QtWidgets.QCheckBox('Anti-windup Enable', self)  # Checkbox para el anti-windup
+        self.pid_button = QtWidgets.QPushButton('Set PID', self)
+        self.pid_button.clicked.connect(self.update_pid)
+
+        self.pid_layout.addWidget(QtWidgets.QLabel("P:"))
+        self.pid_layout.addWidget(self.p_input)
+        self.pid_layout.addWidget(QtWidgets.QLabel("I:"))
+        self.pid_layout.addWidget(self.i_input)
+        self.pid_layout.addWidget(QtWidgets.QLabel("D:"))
+        self.pid_layout.addWidget(self.d_input)
+        self.pid_layout.addWidget(self.windup_checkbox)  # Añadir checkbox al layout
+        self.pid_layout.addWidget(self.pid_button)
+        self.layout.addLayout(self.pid_layout)
+
+        # Gráficos
+        self.win = pg.GraphicsLayoutWidget(show=True)
+        self.plot = self.win.addPlot(row=1, col=1)
+        self.error_plot = self.win.addPlot(row=2, col=1)  # Nuevo gráfico para los errores
+        self.plot.setLabels(left='Position', bottom='Time', title='Reference vs Actual Position')
+        self.error_plot.setLabels(left='Error', bottom='Time', title='Errors Over Time')  # Etiquetas para el gráfico de errores
+        self.plot.addLegend()
+        self.error_plot.addLegend()  # Leyenda para el gráfico de errores
+        self.layout.addWidget(self.win)
+
+        # Curvas para el gráfico de posición
         self.ref_curve = self.plot.plot(pen='r', name='Reference Position')
         self.actual_curve = self.plot.plot(pen='b', name='Actual Position')
 
+        # Curvas para el gráfico de errores
+        self.error_curve = self.error_plot.plot(pen='g', name='Error')
+        self.dedt_curve = self.error_plot.plot(pen='y', name='Error derivative')
+        self.eintegral_curve = self.error_plot.plot(pen='c', name='Error integral')
+
+        # Deques para almacenar datos
         self.data_ref = collections.deque(maxlen=MAX_POINTS)
         self.data_actual = collections.deque(maxlen=MAX_POINTS)
+        self.data_error = collections.deque(maxlen=MAX_POINTS)  # Datos de error
+        self.data_dedt = collections.deque(maxlen=MAX_POINTS)  # Datos de la derivada del error
+        self.data_eintegral = collections.deque(maxlen=MAX_POINTS)  # Datos del error integral
         self.time_stamps = collections.deque(maxlen=MAX_POINTS)
 
         # Configurar timer para actualizar gráfico
@@ -31,56 +84,54 @@ class RealTimeGraph:
         self.timer.timeout.connect(self.update)
         self.timer.start(50)
 
-        self.serial = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
-        time.sleep(2)
-        self.serial.flushInput()
-
-        # Thread para leer datos
-        self.serial_thread = threading.Thread(target=self.read_from_port, args=(self.serial,))
-        self.serial_thread.daemon = True
-        self.serial_thread.start()
-
-        # Variables para guardar los datos leídos
-        self.latest_ref = None
-        self.latest_actual = None
-        self.lock = threading.Lock()
+    def update_pid(self):
+        p = self.p_input.text()
+        i = self.i_input.text()
+        d = self.d_input.text()
+        windup = '1' if self.windup_checkbox.isChecked() else '0'  # Estado del checkbox anti-windup
+        command = f"P{p},I{i},D{d},W{windup}\n"  # Incluir estado del anti-windup en el comando
+        self.serial.write(command.encode())
 
     def read_from_port(self, ser):
         while True:
             line = ser.readline().decode('utf-8').strip()
             if line:
                 data = line.split(" ")
-                if len(data) == 2:
+                if len(data) == 5:  # Asegurarse de que hay cinco elementos: referencia, actual, error, dedt, eintegral
                     with self.lock:
-                        self.latest_ref, self.latest_actual = map(float, data)
+                        self.latest_data = list(map(float, data))  # Almacenar los últimos datos leídos
 
     def update(self):
         with self.lock:
-            if self.latest_ref is not None and self.latest_actual is not None:
+            if self.latest_data is not None:
+                ref, actual, error, dedt, eintegral = self.latest_data
                 current_time = time.time()
-                
-                self.data_ref.append(self.latest_ref)
-                self.data_actual.append(self.latest_actual)
+
+                # Añadir nuevos puntos de datos a los deques
+                self.data_ref.append(ref)
+                self.data_actual.append(actual)
+                self.data_error.append(error)  # Error
+                self.data_dedt.append(dedt)  # Derivada del error
+                self.data_eintegral.append(eintegral)  # Error integral
                 self.time_stamps.append(current_time)
 
+                # Actualizar curvas con nuevos datos
                 self.ref_curve.setData(self.time_stamps, self.data_ref)
                 self.actual_curve.setData(self.time_stamps, self.data_actual)
+                self.error_curve.setData(self.time_stamps, self.data_error)  # Curva de error
+                self.dedt_curve.setData(self.time_stamps, self.data_dedt)  # Curva de la derivada del error
+                self.eintegral_curve.setData(self.time_stamps, self.data_eintegral)  # Curva del error integral
 
-                self.latest_ref, self.latest_actual = None, None  # Resetear los últimos datos leídos
+                self.latest_data = None  # Resetear los últimos datos leídos
 
-    def start(self):
-        if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-            QtWidgets.QApplication.instance().exec_()
+    def closeEvent(self, event):
+        self.serial.close()  # Cerrar conexión serial cuando se cierra la aplicación
 
-    def close(self):
-        self.serial.close()
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    main = RealTimeGraph()
+    main.show()
+    sys.exit(app.exec_())
 
-# Crear y ejecutar la aplicación
 if __name__ == '__main__':
-    graph = RealTimeGraph()
-    try:
-        graph.start()
-    except KeyboardInterrupt:
-        print("Exiting...")
-    finally:
-        graph.close()
+    main()
